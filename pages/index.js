c517567a79ed7f9378fc0e1a0e16ad7f40e1374d
IshanPalkar <ishanpalkar3005@gmail.com>
2022-02-01 12:14:10 +0530

    commit on master

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import link from 'next/link'
+import Header from '../components/Header'
+
+
 
 export default function Home() {
   return (
-    <h1>JODDD</h1>
+    <Header />
   )
 }
